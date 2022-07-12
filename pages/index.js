cb1a76d3e47af1953f786382ee17751903d43dbd
Imad Ottallah <imadotta@gmail.com>
2022-07-12 17:40:58 -0500

    called joke and punchline

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import RenderJoke from '../components/Jokes';
+
 function Home() {
   return (
     <div
@@ -9,7 +11,10 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Welcome to the Jokes!</h1>
+      <h1> </h1>
+      <p>
+        <RenderJoke />
+      </p>
     </div>
   );
 }
